f5ced7153550e3757d6205334e74585f092edee0
Ryan Smith <3045513+ryansmith94@users.noreply.github.com>
2018-03-20 11:51:25 +0000

    fix(deps): Updates to core v7. (#13)
    BREAKING CHANGE: Updates to [core v7](https://github.com/js-entity-repos/core/releases/tag/v7.0.0).

--- src/functions/getEntities.ts
+++ src/functions/getEntities.ts
@@ -1,6 +1,9 @@
 import GetEntities from '@js-entity-repos/core/dist/signatures/GetEntities';
 import Entity from '@js-entity-repos/core/dist/types/Entity';
+import Pagination from '@js-entity-repos/core/dist/types/Pagination';
+import { forward } from '@js-entity-repos/core/dist/types/PaginationDirection';
 import Sort from '@js-entity-repos/core/dist/types/Sort';
+import SortOrder, { asc } from '@js-entity-repos/core/dist/types/SortOrder';
 import createCursorFromEntity from '@js-entity-repos/core/dist/utils/createCursorFromEntity';
 import createPaginationFilter from '@js-entity-repos/core/dist/utils/createPaginationFilter';
 import { first, last, mapValues } from 'lodash';
@@ -11,12 +14,12 @@ const xor = (conditionA: boolean, conditionB: boolean) => {
 };
 
 export default <E extends Entity>(config: FacadeConfig<E>): GetEntities<E> => {
-  const defaultPagination = {
+  const defaultPagination: Pagination = {
     cursor: undefined,
-    forward: true,
+    direction: forward,
     limit: config.defaultPaginationLimit,
   };
-  const defaultSort = { id: true } as Sort<E>;
+  const defaultSort = { id: asc } as Sort<E>;
   return async ({ filter = {}, sort = defaultSort, pagination = defaultPagination }) => {
     const collection = (await config.collection());
 
@@ -24,8 +27,8 @@ export default <E extends Entity>(config: FacadeConfig<E>): GetEntities<E> => {
     const fullFilter = { $and: [filter, paginationFilter] };
     const constructedFilter = config.constructFilter(fullFilter);
     const constructedSort = config.constructSort(sort);
-    const mongoSort = mapValues(constructedSort, (sortValue: boolean) => {
-      return !xor(pagination.forward, sortValue) ? 1 : -1;
+    const mongoSort = mapValues(constructedSort, (sortOrder: SortOrder) => {
+      return !xor(pagination.direction === forward, sortOrder === asc) ? 1 : -1;
     });
 
     const documents = await collection
